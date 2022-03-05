93463434fbd08ef8ae1837c516f73251b3cff0c2
KnutSander <knutsi98@gmail.com>
2022-03-05 16:54:50 +0000

    program can open the archive and close the security pop up

--- image_labeling/README.md
+++ image_labeling/README.md
@@ -1,2 +1,7 @@
 # Image_Labeling
+The aim of this is to search through The Independent technology and science related news, and try to label the images in each article
 Here I will attempt to label images based on the most common word in the article, after formatting and processing it through Elasticsearch.
+
+## Notes
+Only look at pictures if the page contains *.html*, any other pages are not news pages.
+Maybe try to search through the archives, using Selenium.
---
+++ image_labeling/link_extraction.py
@@ -0,0 +1,73 @@
+#!usr/bin/env python3
+
+"""
+Exports page links from The Independent archive
+Only links related to either Tech or Science
+"""
+
+#-------------------------------------------------------------------------------
+# Imports and Global Variables
+#-------------------------------------------------------------------------------
+
+# Standard imports
+import sys, requests, validators, time, csv
+
+# BeautifulSoup
+from bs4 import BeautifulSoup as bs
+
+# Selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
+
+# Webdriver Manager
+from webdriver_manager.firefox import GeckoDriverManager
+
+#-------------------------------------------------------------------------------
+# Class and Routines
+#-------------------------------------------------------------------------------
+
+
+
+#-------------------------------------------------------------------------------
+# Main Program
+#-------------------------------------------------------------------------------
+
+# Initialise driver for selenium
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+
+# Get the archive page
+driver.get("https://www.independent.co.uk/archive")
+
+# Need to wait for the frame to load, test to make sure this enough
+# Better way to do this https://stackoverflow.com/questions/26566799/wait-until-page-is-loaded-with-selenium-webdriver-for-python
+time.sleep(3)
+
+# Switch to pivacy frame
+driver.switch_to.frame('sp_message_iframe_598404')
+
+# Find the buttons and assign the ACCEPT button
+buttons = driver.find_elements(By.TAG_NAME, 'button')
+button = None
+for b in buttons:
+    if b.get_attribute('title') == 'AGREE':
+        button = b
+        break
+
+# Click the accept button
+actions = ActionChains(driver)
+actions.click(button)
+actions.perform()
+
+# Return to the main content frame
+driver.switch_to.default_content()
+
+# Open every year, extract the links and get the links within them that are
+# related to tech and science
+
+#driver.close()
+#-------------------------------------------------------------------------------
+# End of "Program name"
+#-------------------------------------------------------------------------------
