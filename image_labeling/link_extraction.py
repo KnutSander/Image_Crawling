2b358de28dda8942429340f8f19e9b04812b4b9c
KnutSander <knutsi98@gmail.com>
2022-03-07 12:12:52 +0000

    created link_extraction.py, which extracts links related to predefined topics

--- image_labeling/link_extraction.py
+++ image_labeling/link_extraction.py
@@ -45,25 +45,75 @@ class LinkExtractor:
     # Gets all links in the given soup
     # Used on the main archive page to get date links
     def get_links(self, soup):
-        link_objs = soup.get_all('a')
+        link_objs = soup.find_all('a')
         for obj in link_objs:
-            self.date_urls.append(base_url + obj.get('href')
+            self.date_urls.append(base_url + obj.get('href'))
 
 
     # Returns links that are relevant to the topics
+    # sepecefied in the topics list
     def get_rel_links(self, soup):
-        link_objs = soup.get_all('a')
+        link_objs = soup.find_all('a')
         for obj in link_objs:
+                                
             href = obj.get('href')
 
-            if href:
-                                  
+            # Check if the link is on a relevant topic
+            link_relevant = False
+            for topic in topics:
+                if topic in href:
+                    link_relevant = True
+                    break
 
+            # If it isn't relevant the loop moves on to another link
+            if not link_relevant:
+                continue
+
+            # Link is relevant, add it to the list
+            self.rel_urls.append(base_url + href)
+
+
+    # Main crawling method
+    def crawl(self):
+        
+        # Get the links from the starting page
+        main_soup = self.get_content(start_url)
+        self.get_links(main_soup)
+
+        # Depth limit and count
+        max_depth = 31
+        cur_depth = 0
+
+        # Start extracting relevant links
+        while self.date_urls:
+            print("Search depth " + str(cur_depth))
+
+            url = self.date_urls.pop(0)
+            soup = self.get_content(url)
+            self.get_rel_links(soup)
+
+            cur_depth += 1
+            if cur_depth == max_depth:
+                break
+            
+            # Sleep to avoid aggressive crawling
+            time.sleep(0.5)
+
+        self.output()
+
+
+    # Outputs the results into a csv
+    def output(self):
+        with open('links.csv', 'w') as f:
+            for url in self.rel_urls:
+                f.write(url + "\n")
+
+                                  
 #-------------------------------------------------------------------------------
 # Main Program
 #-------------------------------------------------------------------------------
 
-
+LinkExtractor().crawl()
 
 #-------------------------------------------------------------------------------
 # End of link_extraction.py
